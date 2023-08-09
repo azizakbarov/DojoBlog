4900a35e844bbf872240e0c8e3f63c24be0c92d6
Aziz Akbarov <azizbakbarov@gmail.com>
2023-08-09 14:50:42 +0500

    Setting up Route Parameters

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom/cjs/react-router-dom";
+
 const BlogList = ({blogs, title, handleDelete}) => {
     return ( 
         <div className="blog-list">
@@ -5,9 +7,11 @@ const BlogList = ({blogs, title, handleDelete}) => {
              {blogs.map (blog => {
                 return(
                 <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>written by {blog.author}</p>
-                 </div>
+                    <Link to={`/blogs/${blog.id}`}> 
+                        <h2>{blog.title}</h2>
+                        <p>written by {blog.author}</p> 
+                    </Link>
+                </div>
             )})}
         </div>
      );
